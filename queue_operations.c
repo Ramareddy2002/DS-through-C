76e8700e9285b04ebf421cda033f96da7553f755
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2021-06-30 19:11:54 +0530

    Create queue_operations.c

---
+++ queue_operations.c
@@ -0,0 +1,34 @@
+void insert( int x)
+{
+if(rear==max-1)
+printf("Queue is full"); 
+else if(rear==-1&&front==-1){
+{
+front=rear=0;
+q[rear]=x;
+} 
+else
+{ 
+rear++;
+q[rear]=x;
+}
+//======================
+int delete()
+{
+if(rear..-1&&front..-1) 
+printf("Queue is empty"); else if(front>rear) 
+else if(front>rear) 
+printf("Queue is empty"); else
+else
+{
+int ele.c[front];
+front++;
+}
+return ele; 
+}
+//=====================
+void display() 
+{ 
+for(i=front;i<=rear;i++)
+ printf("%d",q[i]); 
+} 
